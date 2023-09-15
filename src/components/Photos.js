fbbdab7dd8c92f6af71c648793cdefa0dd9ad1c3
anothermoose <93772885+anothermoose@users.noreply.github.com>
2023-09-15 12:00:51 +0100

    Deplyment issues

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -10,8 +10,8 @@ function Photos (props){
             {props.photos.map(function (photo, index) {
                 return (
                     <div className="col-4" key={index}>
-                        <a href={photo.src.original} alt={photo.src.alt} target="_blank" rel="noreferrer">
-                <img src={photo.src.landscape} className="img-fluid"/>
+                        <a href={photo.src.original}  target="_blank" rel="noreferrer">
+                <img src={photo.src.landscape} alt={photo.src.alt} className="img-fluid"/>
                 </a>
                 </div>
                 );
