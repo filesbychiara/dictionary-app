462449c464ab7dbbae2569837196d18d30a76699
anothermoose <93772885+anothermoose@users.noreply.github.com>
2023-09-15 11:52:09 +0100

    Added alt tags for accessibility on deployment

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -10,7 +10,7 @@ function Photos (props){
             {props.photos.map(function (photo, index) {
                 return (
                     <div className="col-4" key={index}>
-                        <a href={photo.src.original} target="_blank" rel="noreferrer">
+                        <a href={photo.src.original} alt={photo.src.alt} target="_blank" rel="noreferrer">
                 <img src={photo.src.landscape} className="img-fluid"/>
                 </a>
                 </div>
